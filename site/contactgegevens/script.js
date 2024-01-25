316f7b8e10a101164660b1ece4a2ef1751d1d6d7
Yassine B <yassinemens53@gmail.com>
2024-01-25 11:39:03 +0100

    Silvan: Kleine fix. Wel minder commits dan gepland

---
+++ site/contactgegevens/script.js
@@ -0,0 +1,22 @@
+function toggleDarkMode() {
+    const body = document.body;
+    const header = document.querySelector('header');
+    const navLinks = document.querySelectorAll('nav a');
+    const darkModeButton = document.querySelector('.dark-mode-button');
+    const footer = document.querySelector('.footer');
+    const logo = document.getElementById('logo');
+
+    const isDarkMode = body.classList.toggle('dark-mode');
+    header.classList.toggle('dark-mode', isDarkMode);
+    navLinks.forEach(link => link.classList.toggle('dark-mode', isDarkMode));
+    darkModeButton.classList.toggle('dark-mode', isDarkMode);
+    footer.classList.toggle('dark-mode', isDarkMode);
+
+    if (isDarkMode) {
+        logo.src="./images/logowit.png";
+    } else {
+        logo.src="./images/nxtmuseum.com.png";
+    }
+}
+
+///test
